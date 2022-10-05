1dce5fba4eb8919165218580252803e641b2543e
Roger Willian <williandmn@gmail.com>
2022-10-05 19:39:59 -0300

    Corrige os testes de aceitação

--- acceptance/run.sh
+++ acceptance/run.sh
@@ -9,17 +9,15 @@ for entrada in *_entrada.txt; do
   linhas=$(wc -l ${saida_esperada} | cut -f 1 -d " ")
   echo -n "Rodando o teste de aceitação ${teste}.........."
   java -jar app.jar < ${entrada} | tail -n ${linhas} -- > saida_atual.txt
-  diff -q --strip-trailing-cr ${saida_esperada} saida_atual.txt > diferenca.txt
-  cat diferenca.txt
+  diff -y --strip-trailing-cr ${saida_esperada} saida_atual.txt > diferenca.txt
   if [ $? -ne 0 ]; then
     falhou=1
     echo "FALHOU"
     echo "Erro no teste ${teste}:"
-    echo ">>>>>> ESPERADO"
-    cat ${saida_esperada}
-    echo "======"
-    cat saida_atual.txt
-    echo "<<<<<< OBTIDO"
+    echo -n "             >>>>>> ESPERADO <<<<<<                           "
+    echo -n "              >>>>>> OBTIDO <<<<<<                            "
+    echo ""
+    cat diferenca.txt
   else
     echo "PASSOU"
   fi
