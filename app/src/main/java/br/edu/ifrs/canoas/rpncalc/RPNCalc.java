0391bbd7e732e4bae4b8e7a94692aaa7c55ecbfa
Filipe Mallmann Siota <filipesiota@gmail.com>
2022-10-06 13:58:47 -0300

    FIX(RPNCalc.java): Corrige erro na linha 14

--- app/src/main/java/br/edu/ifrs/canoas/rpncalc/RPNCalc.java
+++ app/src/main/java/br/edu/ifrs/canoas/rpncalc/RPNCalc.java
@@ -11,7 +11,7 @@ public class RPNCalc {
         Stack<Double> pilha = new Stack<>();
         Engine engine = new Engine(pilha);
         Tokenizer tokenizer = new Tokenizer(engine.operacoesValidas());
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
+        // System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
 
         engine.limparTela();
         engine.ajuda();
